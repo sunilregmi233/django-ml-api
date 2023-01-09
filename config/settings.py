6478f237f8e242a9d7d0d30e7f1f14129853b003
sunilregmi233 <sunilregmi233@gmail.com>
2023-01-09 17:48:17 +0545

    python anywhere deployment

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        # 'NAME': root('myblog.db'),                      # Or path to database file if using sqlite3.
+        'NAME': 'API',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'sunilregmi233',
         'PASSWORD': 'Candy@871',
