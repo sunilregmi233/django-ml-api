705079b61e936af34bf6583f86643c397cbdde2d
sunilregmi233 <sunilregmi223@gmail.com>
2023-12-31 12:45:19 +0545

    add poetry and gunicorn

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR + '/var/www/static/',
+    BASE_DIR '/var/www/static/',
 ]
 
 # Default primary key field type
