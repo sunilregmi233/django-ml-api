50585a502caeff0efedfd4a6529212cec105db79
sunilregmi233 <sunilregmi223@gmail.com>
2023-12-31 12:59:17 +0545

    add poetry and gunicorn

--- config/settings.py
+++ config/settings.py
@@ -139,7 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
