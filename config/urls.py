2719f8285132343f15990791e8143d76cafd8abe
sunilregmi233 <sunilregmi223@gmail.com>
2023-12-31 13:07:32 +0545

    add poetry and gunicorn

--- config/urls.py
+++ config/urls.py
@@ -46,6 +46,3 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui( 'swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
